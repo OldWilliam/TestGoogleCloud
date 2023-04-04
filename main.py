1fae97eedba945d48e602e6d9e92857250e13513
weixin <wei.xin@immomo.com>
2023-04-05 02:46:24 +0800

    simple server

---
+++ main.py
@@ -0,0 +1,16 @@
+
+# Here is an example of a Flask server code:
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
+
+# The above code creates a Flask server that listens for incoming requests on port 5000 by default. 
+# When a request is received, the server will call the 'hello_world' function and return the string 'Hello, World!'.
