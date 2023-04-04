50a0c4b6c99e46578cf33a21a9de1c8e68f635e1
weixin <wei.xin@immomo.com>
2023-04-05 03:37:37 +0800

    simple server

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-
 # Here is an example of a Flask server code:
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run(port=8080)
